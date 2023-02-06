5ffc0869f5fd2126f8c6e5acbf0c0fc5cd571113
cristiii <cristian.vlad10@gmail.com>
2023-02-06 14:06:29 +0200

    added producer

---
+++ kafka-basics/src/main/java/com/example/kafkabasics/ProducerDemo.java
@@ -0,0 +1,37 @@
+package com.example.kafkabasics;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        //localhost
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        // conduktor.io
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"CJASG\" password=\"3t223tsdg\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        producer.send(producerRecord);
+
+        producer.flush();
+
+        producer.close();
+    }
+}
