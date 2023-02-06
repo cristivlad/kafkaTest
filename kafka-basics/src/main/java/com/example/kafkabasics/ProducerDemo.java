26477d313ee9b2197a4ee51ad56701ff21750d99
cristiii <cristian.vlad10@gmail.com>
2023-02-06 14:41:05 +0200

    added callback logic to producer

--- kafka-basics/src/main/java/com/example/kafkabasics/ProducerDemo.java
+++ kafka-basics/src/main/java/com/example/kafkabasics/ProducerDemo.java
@@ -1,7 +1,9 @@
 package com.example.kafkabasics;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +13,8 @@ import java.util.Properties;
 public class ProducerDemo {
 
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Properties properties = new Properties();
         //localhost
 //        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
@@ -26,9 +29,24 @@ public class ProducerDemo {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
 
-        producer.send(producerRecord);
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        log.info("Received new metadata \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        log.error("Error while producing!");
+                    }
+                }
+            });
+        }
 
         producer.flush();
 
